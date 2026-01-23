0388e98000ac425b9ef8ca33cebd368e998c60ac
WesleyyD <delano.wesley@academico.ifpb.edu.br>
2026-01-23 04:32:39 -0300

    feat: adiciona model de coordenador ao schema prisma

---
+++ src/routes/coordinatorRoutes.ts
@@ -0,0 +1,16 @@
+import express  from "express";
+import RegisterCoordinatorController  from "../controllers/Coordinator/registerCoordinatorController.ts";
+import UpdateCoordinatorController  from "../controllers/Coordinator/updateCoordinatorController.ts";
+import GetAllCoordinatorController  from "../controllers/Coordinator/getAllCoordinatorController.ts";
+import DeleteCoordinatorController  from "../controllers/Coordinator/deleteCoordinatorController.ts";
+import GetCoordinatorByIdController from "../controllers/Coordinator/getCoordinatorByIdController.ts";
+
+const coordinatorRoutes = express.Router();
+
+coordinatorRoutes.post("/", RegisterCoordinatorController.handle);
+coordinatorRoutes.get("/", GetAllCoordinatorController.handle);
+coordinatorRoutes.get("/:id", GetCoordinatorByIdController.handle);
+coordinatorRoutes.put("/:id", UpdateCoordinatorController.handle);
+coordinatorRoutes.delete("/:id", DeleteCoordinatorController.handle);
+
+export default coordinatorRoutes;
