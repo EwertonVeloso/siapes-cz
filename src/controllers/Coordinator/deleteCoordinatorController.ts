90f739a7aaf4234be7ad658d7a0e8476ccb1b1ae
WesleyyD <delano.wesley@academico.ifpb.edu.br>
2026-01-23 01:23:32 -0300

    feat: adiciona controller de deletar coordenador

---
+++ src/controllers/Coordinator/deleteCoordinatorController.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import DeleteCoordinatorUseCase from "../../domain/Coordinator/usecases/deleteCoordinatorUseCase.ts";
+import { AppError } from "../../errors/appErrors.ts";
+
+class DeleteCoordinatorController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const id = req.params.id as string;
+
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+    if (!uuidRegex.test(id)) {
+      throw new AppError("ID inválido.", 400);
+    }
+
+    const result = await DeleteCoordinatorUseCase.execute(id);
+
+    return res.status(result.status).json(result.body);
+  }
+}
+
+export default new DeleteCoordinatorController();
